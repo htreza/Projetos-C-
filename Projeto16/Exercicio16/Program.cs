ec7ddef641123f315c9539af30cdd65b164fb092
Henrique Treza <trezah.it@outlook.com>
2018-05-11 03:26:03 -0300

    C#

---
+++ Projeto16/Exercicio16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int soma = 0;
+            int contador = 0;
+            while (a >= 0) {
+                Console.WriteLine("Digite um número:");
+                int numero = int.Parse(Console.ReadLine());
+                if (numero >= 0)
+                {
+                    soma += numero;
+                    contador++;
+                }
+                else {
+                    a = numero;
+                }
+            }
+
+            Console.WriteLine("A soma resulta em {0}, a média é {1}, e o total de números lidos é {2}",soma,(soma/contador),contador);
+
+            Console.ReadKey();
+        }
+    }
+}
